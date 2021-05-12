994a6a67aa086304804f6662f1d0084eb68c9800
ironpad_ryujo <70073019+quantum818@users.noreply.github.com>
2021-05-13 02:29:04 +0900

    Create Main.java

---
+++ 21 5 10/src/Main.java
@@ -0,0 +1,90 @@
+/*　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　◆◆◆　　　　　　　　　　　　　　　　　　　　
+　　　　　　　　　　　　　◆◆◆　　　　　　　　　　　　　　　　　◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆　　　　　　　　　　　◆◆　　　　　◆◆◆◆◆◆◆◆◆◆◆◆◆　　　
+　　　　　　　　　　　　　◆◆◆　　　　　　　　　　　　　　　　　◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆　　　　　　　　　　　◆◆　　　　　◆◆◆◆◆◆◆◆◆◆◆◆◆　　　
+　　　　　　　　　　　　　◆◆◆　　　　　　　　　　　　　　　　　◆◆◆　　　　　◆◆　　　◆◆　　　　◆◆◆◆　　　　　　　　　　　◆◆　　　　　◆◆◆◆◆◆◆◆◆◆◆◆◆　　　
+　　　　　　　　　　　　　◆◆◆　　　　　　　　　　　　　　　　　　　　　　　　　◆◆　　　◆◆　　　　　　　　　　　　　　　　　　　◆◆　　　　　◆◆　◆◆◆◆◆◆　◆◆　　　　
+　　　　　　　　　　　　　◆◆◆　　　　　　　　　　　　　　　　　　◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆　　　　　　　　◆◆◆◆◆◆◆◆◆◆　◆◆　◆◆◆◆◆◆　◆◆　　　　
+　　　　　　　　　　　　　◆◆◆　　　　　　　　　　　　　　　　　　　◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆　　　　　　　　◆◆◆◆◆◆◆◆◆◆　◆◆　◆◆◆◆◆◆　◆◆　　　　
+　　　　　　◆◆◆◆　　　◆◆◆　　◆◆◆　　　　　　　　　　　　　　◆◆◆　　　◆◆　　　◆◆　　　◆◆◆◆　　　　　　　　◆◆　　◆◆　　◆◆　◆◆　◆◆◆◆◆◆　◆◆　　　　
+　　　　　　◆◆◆◆　　　◆◆◆　　◆◆◆◆　　　　　　　　　　　　　◆◆◆　　　◆◆　　　◆◆　　　◆◆◆◆　　　　　　　　◆◆　　◆◆　　◆◆　◆◆　◆◆◆◆◆◆　◆◆◆　　　
+　　　　　　◆◆◆　　　　◆◆◆　　◆◆◆◆　　　　　　　　　　　　　◆◆◆　　　◆◆　　　◆◆　　　◆◆◆◆　　　　　　　　◆◆　　◆◆　　◆◆　◆◆　◆◆◆◆◆◆　◆◆◆　　　
+　　　　　◆◆◆◆　　　　◆◆◆　　　◆◆◆◆　　　　　　　　　　　　◆◆◆　　　◆◆　　　◆◆　　　◆◆◆◆　　　　　　　　◆◆　　◆◆　　◆◆　　　　　　　　　　　　　　　　　
+　　　　　◆◆◆◆　　　　◆◆◆　　　◆◆◆◆　　　　　　　　　　　　◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆　　　　　　　　◆◆　　◆◆　　◆◆　　◆◆◆◆◆◆◆◆◆◆◆　　　　
+　　　　　◆◆◆　　　　　◆◆◆　　　　◆◆◆◆　　　　　　　　　　◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆　　　　　　　　◆◆　　◆◆　　◆◆　　◆◆◆◆◆◆◆◆◆◆◆　　　　
+　　　　◆◆◆◆　　　　　◆◆◆　　　　◆◆◆◆　　　　　　　　　　　　　　　　　　　◆◆◆　　　　　　　　　　　　　　　　　◆◆　　◆◆　　◆◆　　◆◆　　　　　　　◆◆　　　　
+　　　　◆◆◆　　　　　　◆◆◆　　　　　◆◆◆◆　　　　　　　◆◆◆◆◆　　　　　　　◆◆　　　　　　　◆◆◆◆　　　　　　◆◆　　◆◆　　◆◆　　◆◆◆◆◆◆◆◆◆◆◆　　　　
+　　　◆◆◆◆　　　　　　◆◆◆　　　　　◆◆◆◆　　　　　　　◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆　　　　　　◆◆　　◆◆　　◆◆　　◆◆◆◆◆◆◆◆◆◆◆　　　　
+　　　◆◆◆◆　　　　　　◆◆◆　　　　　　◆◆◆　　　　　　　◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆　　　　　　◆◆　　◆◆　◆◆◆　　◆◆　　　　　　　◆◆　　　　
+　　◆◆◆◆　　　　　　　◆◆◆　　　　　　◆◆◆◆　　　　　　　　　　　　　　◆◆◆◆◆◆◆◆◆　　　　　　　　　　　　　　◆◆　　◆◆◆◆◆◆　　◆◆　　　　　　　◆◆　　　　
+　　　◆◆◆　　　　　　　◆◆◆　　　　　　　◆　　　　　　　　　　　　　　　◆◆◆◆◆◆◆◆◆◆◆◆　　　　　　　　　　　　◆◆　　◆◆◆◆◆◆　　◆◆◆◆◆◆◆◆◆◆◆　　　　
+　　　　　　　　　　　　　◆◆◆　　　　　　　　　　　　　　　　　　　　　◆◆◆◆◆◆　◆◆　◆◆◆◆◆◆　　　　　　　　　　◆◆　　◆◆◆◆◆　　　◆◆◆◆◆◆◆◆◆◆◆　　　　
+　　　　　　　　　　　　　◆◆◆　　　　　　　　　　　　　　　　　　　◆◆◆◆◆◆◆　　◆◆　　◆◆◆◆◆◆◆◆◆　　　　　　　　　　◆◆　　　　　　◆◆　　　　　　　◆◆　　　　
+　　　　　　　　　　　　◆◆◆◆　　　　　　　　　　　　　　　　◆◆◆◆◆◆◆◆　　　　◆◆　　　◆◆◆◆◆◆◆◆　　　　　　　　　　◆◆　　　　　　◆◆◆◆◆◆◆◆◆◆◆　　　　
+　　　　　　　　　◆◆◆◆◆◆◆　　　　　　　　　　　　　　　　◆◆◆◆◆◆◆　　　　◆◆◆◆　　　　◆◆◆◆◆　　　　　　　　　　　◆◆　　　　　　◆◆◆◆◆◆◆◆◆◆◆　　　　
+　　　　　　　　　　◆◆◆◆◆　　　　　　　　　　　　　　　　　　◆◆◆◆　　　　　　◆◆◆◆　　　　　　　◆◆　　　　　　　　　　　◆◆　　　　　　◆◆　　　　　　　◆◆　　　　
+　　　　　　　　　　◆◆◆◆　　　　　　　　　　　　　　　　　　　　　　　　　　　　　◆◆◆◆　　　　　　　　　　　　　　　　　　　　◆◆　　　　　　◆◆　　　　　　　　　　　　　
+　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　*/
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        int k = read.nextInt();
+        int count, p;
+        for (int i = 0; i < k; i++) {
+            ArrayList<Integer> homework = new ArrayList();
+            count = 0;
+            int all = read.nextInt();
+            int now = read.nextInt();
+            int alll = all - 1;
+            for (int j = 0; j < all; j++) {
+                int imp = read.nextInt();
+                homework.add(imp);
+            }
+            Iterator<Integer> hw = homework.iterator();
+            dep: while (hw.hasNext()) {
+                int nowc = hw.next();
+                int maxx = Collections.max(homework);
+                int delnow = homework.indexOf(maxx);
+
+                count++;
+                if (delnow == now) {
+                    break dep;
+                } else {
+                    p = 0;
+
+                    if (delnow != 0) {
+                        while (delnow != 0) {
+                            if (p == now) {
+                                now = alll;
+                            } else {
+                                now--;
+                            }
+                            int ttemp = homework.get(p);
+                            homework.remove(p);
+                            homework.add(ttemp);
+                            delnow--;
+
+                        }
+
+                    } else {
+
+                        if (p == now) {
+                            now = alll;
+                        }
+
+                    }
+                    homework.remove(delnow);
+                    alll--;
+                    now--;
+                }
+                hw = homework.iterator();
+            }
+            System.out.println(count);
+        }
+        read.close();
+    }
+}
